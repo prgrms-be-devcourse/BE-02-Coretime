950a4cda8211dd66ea79b77da34193edeec89260
jki503 <jki503@naver.com>
2022-06-22 14:21:40 +0900

    ✅ feat : Comment Entity 적용 안된 사항 재커밋

--- src/main/java/com/prgrms/coretime/comment/domain/Comment.java
+++ src/main/java/com/prgrms/coretime/comment/domain/Comment.java
@@ -1,5 +1,67 @@
 package com.prgrms.coretime.comment.domain;
 
-public class Tmp {
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_id")
+  private Comment parent;
+
+  @OneToMany(mappedBy = "parent", orphanRemoval = false)
+  private List<Comment> children = new ArrayList<>();
+
+  // temp column : user
+  @Column(name = "user_id")
+  private Long userId;
+
+  // temp column : post
+  @Column(name = "post_id")
+  private Long postId;
+
+  @Column(name = "anonymous_seq")
+  private Integer anonymousSeq;
+
+  @Column(name = "content", nullable = false, length = 300)
+  private String content;
+
+  @Column(name = "is_deleted", nullable = false)
+  private Boolean isDelete;
+
+  //TODO: 생성자 처리 어떻게 할 건지, RequestDto 말고 여기서 Dto 받아서 생성할 건지 고민
+
+  /**
+   * TODO : 대댓글 연관관계 설정 1:N 양방향
+   * */
+
+  /**
+   * TODO : USER 1:N 일단 단방향
+   * */
+
+  /**
+   * TODO : POST 1:N 단방향
+   * */
+
+  /***
+   * TODO : validation
+   */
 }
