eb099d0e9bfea1396f10ef1b9cfdfc9251c0a14e
park0503 <hooo0503@gmail.com>
2022-06-23 14:21:50 +0900

    :card_file_box: refactor: post request dto 에 bean validation 추가

--- src/main/java/com/prgrms/coretime/post/dto/request/PostUpdateRequest.java
+++ src/main/java/com/prgrms/coretime/post/dto/request/PostUpdateRequest.java
@@ -3,9 +3,12 @@ package com.prgrms.coretime.post.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class PostUpdateRequest {
+    @NotBlank
     private String title;
     private String content;
 }
