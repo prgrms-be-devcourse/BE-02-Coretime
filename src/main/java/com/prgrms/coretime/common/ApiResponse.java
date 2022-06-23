3cfd9c35a26c81c01f0e569ada6b9c8d00f84ae6
hong yuseok <jericoh@naver.com>
2022-06-23 19:17:04 +0900

    ✅ feat : ApiResponse클래스에 @JsonInclude(Include.NON_NULL) 추가

--- src/main/java/com/prgrms/coretime/common/ApiResponse.java
+++ src/main/java/com/prgrms/coretime/common/ApiResponse.java
@@ -1,8 +1,11 @@
 package com.prgrms.coretime.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(Include.NON_NULL)
 public class ApiResponse<T> {
   private String message;
   private T data;
