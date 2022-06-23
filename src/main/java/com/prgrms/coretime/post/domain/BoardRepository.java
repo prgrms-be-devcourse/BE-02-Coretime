5b3892fcd75e24e20639e5821d91ce8bc81622b3
park0503 <hooo0503@gmail.com>
2022-06-23 10:44:43 +0900

    :white_check_mark: feat: post repository 생성 및 쿼리 작성

---
+++ src/main/java/com/prgrms/coretime/post/domain/BoardRepository.java
@@ -0,0 +1,6 @@
+package com.prgrms.coretime.post.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
---
+++ src/main/java/com/prgrms/coretime/post/domain/PostRepository.java
@@ -0,0 +1,36 @@
+package com.prgrms.coretime.post.domain;
+
+import com.prgrms.coretime.comment.domain.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query(
+            value = "select p from Post p join fetch p.board join fetch p.user where p.board.id = :boardId",
+            countQuery = "select p from Post p where p.board.id = :boardId"
+    )
+    Page<Post> findPostsByBoardId(@Param("boardId") Long boardId, Pageable pageable);
+
+    @Query(
+            value = "select p from Post p join fetch p.board join fetch p.user where p.user.id = :userId",
+            countQuery = "select p from Post p where p.user.id = :userId"
+    )
+    Page<Post> findPostsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("select p from Post p join fetch p.board join fetch p.user where p.id = :postId")
+    Post findPostById(@Param("postId") Long postId);
+
+    @Query("select c from Comment c join fetch c.user where c.post.id = :postId")
+    Page<Comment> findCommentsByPost(@Param("postId") Long postId, Pageable pageable);
+
+    @Query(value = "select p from Post p join fetch p.board join fetch p.user where p.title like :keyword or p.content like :keyword",
+            countQuery = "select p from Post p where p.title like :keyword or p.content like :keyword")
+    Page<Post> searchPosts(@Param("keyword") String keyword, Pageable pageable);
+
+    @Query(value = "select p from Post p join fetch p.board join fetch p.user where p.board.id = :boardId and (p.title like :keyword or p.content like :keyword)",
+    countQuery = "select p from Post p where p.board.id = :boardId and (p.title like :keyword or p.content like :keyword)")
+    Page<Post> searchPostsAtBoard(@Param("keyword") String keyword, @Param("boardId") Long boardId, Pageable pageable);
+}
