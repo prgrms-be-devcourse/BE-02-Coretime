3114dec7d481fd7c9f7f9d45cd6a02608aa3d846
hong yuseok <jericoh@naver.com>
2022-06-24 14:36:48 +0900

    ✅ feat : NotFoundException 추가 및 이에 대한 exceptionHandler 구현 & exceptionHandler에 로거 추가

--- src/main/java/com/prgrms/coretime/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/coretime/common/error/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.prgrms.coretime.common.error;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
@@ -8,27 +9,38 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
   // RequestBody
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    log.warn(e.getMessage());
     return ResponseEntity.badRequest().body("");
   }
 
   // ModelAttribute
   @ExceptionHandler(org.springframework.validation.BindException.class)
   public ResponseEntity<String> handleBindException(BindException e) {
+    log.warn(e.getMessage());
     return ResponseEntity.badRequest().body("");
   }
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
   public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    log.warn(e.getMessage());
     return ResponseEntity.badRequest().body("");
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+    log.warn(e.getMessage());
+    return ResponseEntity.badRequest().body("");
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
+    log.warn(e.getMessage());
     return ResponseEntity.badRequest().body("");
   }
 }
