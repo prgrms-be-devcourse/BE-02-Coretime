9c85a10f0f19ccbd6e142f08145670ce937de7d5
hong yuseok <jericoh@naver.com>
2022-06-25 22:22:55 +0900

    ✅ feat : TimetableController에서 시간표 조회 메서드 구현

--- src/main/java/com/prgrms/coretime/timetable/controller/TimetableController.java
+++ src/main/java/com/prgrms/coretime/timetable/controller/TimetableController.java
@@ -3,6 +3,7 @@ package com.prgrms.coretime.timetable.controller;
 import com.prgrms.coretime.common.ApiResponse;
 import com.prgrms.coretime.timetable.domain.Semester;
 import com.prgrms.coretime.timetable.dto.request.TimetableCreateRequest;
+import com.prgrms.coretime.timetable.dto.response.TimetableResponse;
 import com.prgrms.coretime.timetable.dto.response.TimetablesResponse;
 import com.prgrms.coretime.timetable.service.TimetableService;
 import io.swagger.annotations.Api;
@@ -12,6 +13,7 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,10 +41,22 @@ public class TimetableController {
 
   @ApiOperation(value = "시간표 목록 조회", notes = "연도와 학기에 따른 시간표 목록을 조회합니다.")
   @GetMapping
-  public ResponseEntity<ApiResponse> getTimetables(@RequestParam Integer year, @RequestParam Semester semester) {
+  public ResponseEntity<ApiResponse<TimetablesResponse>> getTimetables(@RequestParam Integer year, @RequestParam Semester semester) {
     TimetablesResponse timetablesResponse = timetableService.getTimetables(year, semester);
 
     ApiResponse apiResponse = new ApiResponse("시간표 목록 조회 완료", timetablesResponse);
+
+    return ResponseEntity
+        .ok()
+        .body(apiResponse);
+  }
+
+  @ApiOperation(value = "시간표 조회", notes = "전달된 timetableId에 따라 시간표를 전달한다.")
+  @GetMapping("/{timetableId}")
+  public ResponseEntity<ApiResponse<TimetableResponse>> getTimetable(@PathVariable Long timetableId) {
+    TimetableResponse timetableResponse = timetableService.getTimetable(timetableId);
+
+    ApiResponse apiResponse = new ApiResponse("시간표 조회 완료", timetableResponse);
 
     return ResponseEntity
         .ok()
