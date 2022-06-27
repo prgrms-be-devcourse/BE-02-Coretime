d0139c1a694b9591300d11d538b09285dab43b2f
hong yuseok <jericoh@naver.com>
2022-06-27 19:08:41 +0900

    🗃️ refactor : 사용하지 않는 메서드 제거

--- src/main/java/com/prgrms/coretime/timetable/controller/LectureController.java
+++ src/main/java/com/prgrms/coretime/timetable/controller/LectureController.java
@@ -1,16 +1,9 @@
 package com.prgrms.coretime.timetable.controller;
 
-import com.prgrms.coretime.common.ApiResponse;
-import com.prgrms.coretime.timetable.dto.request.OfficialLecturesCreateRequest;
 import com.prgrms.coretime.timetable.service.LectureService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,17 +14,4 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class LectureController {
   private final LectureService lectureService;
-
-  @ApiOperation(value = "official 강의 추가", notes = "official 강의들을 추가합니다.")
-  @PostMapping("/officials")
-  public ResponseEntity<ApiResponse> addOfficialLectures(@RequestBody @Valid OfficialLecturesCreateRequest officialLecturesCreateRequest) {
-    lectureService.addOfficialLectures(officialLecturesCreateRequest);
-
-    ApiResponse apiResponse = new ApiResponse("official 강의 추가 완료");
-
-    return ResponseEntity
-        .created(null)
-        .body(apiResponse);
-  }
-
 }
--- src/main/java/com/prgrms/coretime/timetable/service/LectureService.java
+++ src/main/java/com/prgrms/coretime/timetable/service/LectureService.java
@@ -1,13 +1,7 @@
 package com.prgrms.coretime.timetable.service;
 
-import com.prgrms.coretime.timetable.dto.request.OfficialLecturesCreateRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class LectureService {
-  @Transactional
-  public void addOfficialLectures(OfficialLecturesCreateRequest officialLecturesCreateRequest) {
-
-  }
 }
