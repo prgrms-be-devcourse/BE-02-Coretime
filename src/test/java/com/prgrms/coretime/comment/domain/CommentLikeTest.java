7168c7484994a883718cc334f3c89a985ab12992
jki503 <jki503@naver.com>
2022-06-28 12:02:09 +0900

    ✅ feat : CommentLike 작성 및 인스턴스 생성 테스트 코드까지

---
+++ src/test/java/com/prgrms/coretime/comment/domain/CommentLikeTest.java
@@ -0,0 +1,74 @@
+package com.prgrms.coretime.comment.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.prgrms.coretime.post.domain.Post;
+import com.prgrms.coretime.user.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class CommentLikeTest {
+
+  private User user = new User("example@email.com", "example");
+
+  private Post post = Post.builder()
+      .title("title")
+      .content("게시글 내용입니다.")
+      .isAnonymous(true)
+      .build();
+
+  private String content = "이것은 공백도 아니며, 300바이트를 넘지 않아요!";
+
+  private Comment comment = Comment.builder()
+      .user(user)
+      .post(post)
+      .parent(null)
+      .isAnonymous(true)
+      .content(content)
+      .build();
+
+  @Nested
+  @DisplayName("edge case 중에서 ")
+  class Describe_EdgeCase {
+
+    @Test
+    @DisplayName("user가 null일 경우")
+    public void testUserNull() {
+      Assertions.assertThatThrownBy(this::callUserNull)
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("사용자는 필수입니다.");
+    }
+
+
+    @Test
+    @DisplayName("comment가 null일 경우")
+    public void testCommentNull() {
+      Assertions.assertThatThrownBy(this::callCommentNull)
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("댓글은 필수입니다.");
+    }
+
+    private void callUserNull() {
+      new CommentLike(null, comment);
+    }
+
+    private void callCommentNull() {
+      new CommentLike(user, null);
+    }
+  }
+
+  @Nested
+  @DisplayName("happy path 중에서")
+  class Describe_HappyPath {
+
+    @Test
+    @DisplayName("user와 comment 모두 존재 할 경우에는 통과!")
+    public void testCorrectCreation() {
+      CommentLike commentLike = new CommentLike(user, comment);
+      assertThat(commentLike).isInstanceOf(CommentLike.class);
+    }
+
+  }
+}
