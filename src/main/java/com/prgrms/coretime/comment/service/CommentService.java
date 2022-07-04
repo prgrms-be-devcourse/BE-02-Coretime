eb269bc24f9960c25629532bd3d985858f511867
jki503 <jki503@naver.com>
2022-07-04 13:59:42 +0900

    :bug: fix : 생성 및 삭제 로직 수정.. 익명 댓글이 있는지를 확인해야한다

--- src/main/java/com/prgrms/coretime/comment/service/CommentService.java
+++ src/main/java/com/prgrms/coretime/comment/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.prgrms.coretime.comment.service;
 
+import com.mysema.commons.lang.Assert;
 import com.prgrms.coretime.comment.domain.Comment;
 import com.prgrms.coretime.comment.domain.repository.CommentRepository;
 import com.prgrms.coretime.comment.dto.request.CommentCreateRequest;
@@ -9,6 +10,7 @@ import com.prgrms.coretime.common.error.exception.NotFoundException;
 import com.prgrms.coretime.post.domain.Post;
 import com.prgrms.coretime.post.domain.repository.PostRepository;
 import com.prgrms.coretime.user.domain.User;
+import com.prgrms.coretime.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,43 +20,63 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CommentService {
 
+  private final UserRepository userRepository;
   private final PostRepository postRepository;
   private final CommentRepository commentRepository;
 
-  public CommentCreateResponse createComment(CommentCreateRequest commentCreateRequest) {
-    User user = new User("example.com", "it is unsafeUser"); // 이부분이 로그인 받아온 user가 되어야함.
+  // 1. 생성할때 기존 유저가 익명 댓글을 썼는지 확인.
+  // 2. 익명댓글을 썼다면 기존 유저가 사용한 익명 번호를 가져와야함.
+  // 3. 처음 익명댓글을 쓰는 거라면 post에서 가져오기.
+  public CommentCreateResponse createComment(Long userId,
+      CommentCreateRequest commentCreateRequest) {
+    User currentUser = getCurrentUser(userId);
     Post post = getPost(commentCreateRequest.getPostId());
     Comment parent = commentCreateRequest.getParentId() == null ?
         null : getComment(commentCreateRequest.getParentId());
 
+    // 기존에 유저가 댓글을 썼다면 익명번호 가져오기
+    Integer seq = Integer.valueOf(
+        commentRepository.findFirstByUserIdAndPostId(userId, post.getId())
+            .map(comment -> comment.getAnonymousSeq())
+            .orElse(post.getAnonymousSeqAndAdd()));
+
     Comment comment = Comment.builder()
-        .user(user)
+        .user(currentUser)
         .post(post)
         .parent(parent)
         .isAnonymous(commentCreateRequest.getIsCommentAnonymous())
+        .anonymousSeq(seq)
         .content(commentCreateRequest.getContent())
         .build();
 
     commentRepository.save(comment);
 
-    return CommentCreateResponse.of(user, post, comment);
+    return CommentCreateResponse.of(currentUser, post, comment);
   }
 
-  public void deleteComment(Long commentId) {
+  public void deleteComment(Long userId, Long commentId) {
     Comment comment = getComment(commentId);
+    checkValidUser(userId, commentId);
     comment.updateDelete();
   }
 
-  /**
-   * TODO : Exception Message 관리 어떻게 할 것인지
-   */
   private Post getPost(Long postId) {
     return postRepository.findById(postId)
-        .orElseThrow(() -> new NotFoundException(ErrorCode.INVALID_INPUT_VALUE));
+        .orElseThrow(() -> new NotFoundException(ErrorCode.POST_NOT_FOUND));
   }
 
   private Comment getComment(Long commentId) {
     return commentRepository.findById(commentId)
-        .orElseThrow(() -> new NotFoundException(ErrorCode.INVALID_INPUT_VALUE));
+        .orElseThrow(() -> new NotFoundException(ErrorCode.COMMENT_NOT_FOUND));
   }
+
+  private User getCurrentUser(Long userId) {
+    return userRepository.findById(userId)
+        .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+  }
+
+  private void checkValidUser(Long currentUserId, Long targetUserId) {
+    Assert.isFalse(currentUserId == targetUserId, ErrorCode.BAD_REQUEST.getMessage());
+  }
+
 }
