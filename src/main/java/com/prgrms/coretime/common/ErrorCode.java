7470b937572aa6725c5cf7636fa521a6bf53cf2d
jki503 <jki503@naver.com>
2022-07-04 16:32:00 +0900

    🐛 fix : ErroCode 병합 과정에서 세미 콜론 누락

--- src/main/java/com/prgrms/coretime/common/ErrorCode.java
+++ src/main/java/com/prgrms/coretime/common/ErrorCode.java
@@ -49,9 +49,9 @@ public enum ErrorCode {
 
   /**
    * Timetable Domain
-   * */
+   */
   DUPLICATE_TIMETABLE_NAME(400, "T001", "이미 사용중인 이름입니다."),
-  TIMETABLE_NOT_FOUND(404, "T002", "시간표를 찾을 수 없습니다.");
+  TIMETABLE_NOT_FOUND(404, "T002", "시간표를 찾을 수 없습니다."),
 
   /**
    * Comment Domain
