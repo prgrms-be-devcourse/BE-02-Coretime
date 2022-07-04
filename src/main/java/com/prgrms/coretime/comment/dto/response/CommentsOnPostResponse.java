8af50c9b219a067a8dfa0e196afc2d11507188c8
jki503 <jki503@naver.com>
2022-07-05 06:34:37 +0900

    🗃 refactor : 계층형 댓글 response 생성에 대한 책임 repository에서 response로 이동

--- src/main/java/com/prgrms/coretime/comment/dto/response/CommentsOnPostResponse.java
+++ src/main/java/com/prgrms/coretime/comment/dto/response/CommentsOnPostResponse.java
@@ -1,5 +1,7 @@
 package com.prgrms.coretime.comment.dto.response;
 
+import com.prgrms.coretime.comment.domain.Comment;
+import com.prgrms.coretime.user.domain.User;
 import com.querydsl.core.annotations.QueryProjection;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +18,7 @@ public class CommentsOnPostResponse extends CommentResponse {
       Long userId,
       Long parentId,
       Long commentId,
-      Long like,
+      Integer like,
       String name,
       String content
   ) {
@@ -26,4 +28,27 @@ public class CommentsOnPostResponse extends CommentResponse {
   public void setChildren(List<CommentsOnPostResponse> children) {
     this.children = children;
   }
+
+  public static CommentsOnPostResponse of(Comment comment) {
+    User user = comment.getUser();
+    Comment parent = comment.getParent();
+    String name = "익명" + comment.getAnonymousSeq();
+    if (user == null)
+      name = "(알 수 없음)";
+
+    if (user != null && !comment.getIsAnonymous())
+      name = user.getNickname();
+
+    String content = comment.getIsDelete() || user == null ? "삭제된 댓글입니다." : comment.getContent();
+
+    return new CommentsOnPostResponse(
+        user == null ? null : user.getId(),
+        parent == null ? null : parent.getId(),
+        comment.getId(),
+        comment.getLikes().size(),
+        name,
+        content
+    );
+  }
+
 }
