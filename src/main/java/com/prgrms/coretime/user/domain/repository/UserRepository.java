8f0401f95938ab178d8f05c96dcf6b616091d159
julie0005 <julie0005@ajou.ac.kr>
2022-07-05 19:46:15 +0900

    :card_file_box: refactor : unimport 제거

--- src/main/java/com/prgrms/coretime/user/domain/repository/UserRepository.java
+++ src/main/java/com/prgrms/coretime/user/domain/repository/UserRepository.java
@@ -3,9 +3,7 @@ package com.prgrms.coretime.user.domain.repository;
 import com.prgrms.coretime.user.domain.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
