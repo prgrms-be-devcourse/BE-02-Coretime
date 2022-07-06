2730e6ef6b25bd124a8a99a3fc1e5c70747ea62d
HyeonseoJUNG <windymay1920@gmail.com>
2022-07-07 02:34:41 +0900

    :card_file_box: refactor : Message Entity에 생성자 및 validation 추가

--- src/main/java/com/prgrms/coretime/message/domain/Message.java
+++ src/main/java/com/prgrms/coretime/message/domain/Message.java
@@ -2,6 +2,8 @@ package com.prgrms.coretime.message.domain;
 
 import com.prgrms.coretime.common.entity.BaseEntity;
 import com.prgrms.coretime.user.domain.TestUser;
+import com.prgrms.coretime.user.domain.User;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,8 +14,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @Entity
 @Table(name = "message")
@@ -36,5 +40,21 @@ public class Message extends BaseEntity {
 
   @Column(name = "content", nullable = false, length = 300)
   private String content;
+
+  @Builder
+  public Message(MessageRoom messageRoom, TestUser writer, String content) {
+    Assert.notNull(messageRoom, "messageRoom은 null이 아니여야 합니다.");
+    Assert.notNull(writer, "writer는 null이 아니여야 합니다.");
+    validateContent(content);
+    this.messageRoom = messageRoom;
+    this.writer = writer;
+    this.content = content;
+  }
+
+  private void validateContent(String content) {
+    Assert.notNull(content, "메세지 내용은 비어있을 수 없습니다.");
+    Assert.isTrue(content.length() <= 300,
+        "메세지 길이는 300자 이하여야 합니다.");
+  }
 
 }
