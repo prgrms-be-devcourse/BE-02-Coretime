9224705916fea61a5f4c9d68748c6cf844d2ef09
parksang <hooo0503@gmail.com>
2022-07-07 13:42:38 +0900

    :card_file_box: refactor: s3 uploader 수정 및 컨트롤러 파일 테스트 원복

--- src/main/java/com/prgrms/coretime/common/util/AmazonS3Uploader.java
+++ src/main/java/com/prgrms/coretime/common/util/AmazonS3Uploader.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import org.slf4j.Logger;
@@ -60,7 +61,8 @@ public class AmazonS3Uploader {
 
   // 로컬에 파일 업로드 하기
   private Optional<File> convert(MultipartFile file) throws IOException {
-    File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
+    String originalFilename = Objects.requireNonNull(file.getOriginalFilename()).isBlank() ? UUID.randomUUID().toString() : file.getOriginalFilename();
+    File convertFile = new File(System.getProperty("user.dir") + "/" + originalFilename);
     if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
       try (FileOutputStream fos = new FileOutputStream(
           convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
